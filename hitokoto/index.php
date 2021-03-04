211431f7d229c90ee52e9e8cfbc91d002dc006b1
anyfan <study@anyfan.top>
2021-03-04 18:07:20 +0800

    hitokoto🎨

--- hitokoto/index.php
+++ hitokoto/index.php
@@ -6,7 +6,7 @@ $cat = $_GET['cat'];
 $len = (int)$_GET['len'];
 $charset = $_GET['charset'];
 $encode = $_GET['encode'];
-$debug = (int)$_GET['debug'];
+// $debug = (int)$_GET['debug'];
 
 
 if (($cat != 'h') && ($cat != 'd') && ($cat != 'j') && ($cat != 'y')) {
@@ -16,18 +16,6 @@ if (($cat != 'h') && ($cat != 'd') && ($cat != 'j') && ($cat != 'y')) {
 }
 $db = new db;
 $data = $db->get($type, $len);
-
-if ($debug) {
-    echo json_encode($data);
-} else {
-    // 格式化判断，输出js或纯文本
-    if ($encode === 'js') {
-        echo "function hitokoto(){document.write(\"" . $data['hitokoto'] . "\");}";
-    } else {
-        echo $data['hitokoto'];
-    }
-}
-
 
 //编码判断，用于输出相应的响应头部编码
 // if (isset($_GET['charset']) && !empty($_GET['charset'])) {
@@ -39,9 +27,17 @@ if ($debug) {
 //     $charset = 'utf-8';
 // }
 
-// 格式化判断，输出js或纯文本
-// if ($encode === 'js') {
-//     echo "function hitokoto(){document.write('" . $content ."');}";
-// } else {
-//     echo $content;
-// }
+// 格式化判断，输出js,纯文本,json
+if ($encode) {
+    if ($encode === 'js') {
+        echo "function hitokoto(){document.write(\"" . $data['hitokoto'] . "\");}";
+    }
+    if ($encode === 'json') {
+        echo json_encode($data);
+    }
+    if ($encode === 'text') {
+        echo $data['hitokoto'];
+    }
+} else {
+    echo $data['hitokoto'];
+}
